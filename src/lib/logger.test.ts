043f59c65d3b440c00248617269e8d745264fd9d
Romedix1 <doboszmichal4@gmail.com>
2026-07-13 23:08:45 +0200

    test: logger function

---
+++ src/lib/logger.test.ts
@@ -0,0 +1,55 @@
+import { createLog } from '@/lib/logger'
+import { prisma } from '@/lib/prisma'
+
+vi.mock('@/lib/prisma', () => ({
+  prisma: {
+    systemLog: {
+      create: vi.fn(),
+    },
+  },
+}))
+
+describe('System logger', () => {
+  it('Should correctly create log ', async () => {
+    await createLog('User log in', 'Corretly logged in', 'user_123')
+
+    expect(prisma.systemLog.create).toHaveBeenCalledTimes(1)
+    expect(prisma.systemLog.create).toHaveBeenCalledWith({
+      data: {
+        action: 'User log in',
+        details: 'Corretly logged in',
+        userId: 'user_123',
+      },
+    })
+  })
+
+  it('Should handle undefined parameters', async () => {
+    await createLog('User logged out')
+
+    expect(prisma.systemLog.create).toHaveBeenCalledTimes(1)
+    expect(prisma.systemLog.create).toHaveBeenCalledWith({
+      data: {
+        action: 'User logged out',
+        details: undefined,
+        userId: undefined,
+      },
+    })
+  })
+
+  it('Should silently fail if prisma throws an error', async () => {
+    vi.stubEnv('NODE_ENV', 'development')
+
+    vi.mocked(prisma.systemLog.create).mockRejectedValueOnce(
+      new Error('Network error'),
+    )
+
+    const consoleSpy = vi.spyOn(console, 'error').mockImplementation(() => {})
+
+    await createLog('Deleted build')
+
+    expect(consoleSpy).toHaveBeenCalled()
+
+    consoleSpy.mockRestore()
+    vi.unstubAllEnvs()
+  })
+})
