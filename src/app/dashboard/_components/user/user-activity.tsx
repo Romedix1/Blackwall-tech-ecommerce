8238f055c31a398286e77b0ccae8c9f66fc5c845
Romedix1 <doboszmichal4@gmail.com>
2026-07-15 16:47:15 +0200

    feat: add user logs to dashboard

--- src/app/dashboard/_components/user/user-activity.tsx
+++ src/app/dashboard/_components/user/user-activity.tsx
@@ -15,56 +15,51 @@ export const UserActivity = async () => {
 
   const isAdmin = userRole === 'admin'
 
-  const userActivity = [
-    {
-      id: '1',
-      action: 'UPLINK_ESTABLISHED',
-      date: '2026-02-20',
-    },
-    {
-      id: '2',
-      action: 'PAYMENT_ACCEPTED',
-      date: '2026-02-18',
-    },
-    { id: '3', action: 'SCHEMATIC_SAVED', date: '2026-02-18' },
-  ]
-  // TODO: ADD ACTIVITY TRACKER
-
-  //   const userActivity = prisma.activity.findMany({
-  //     where: { user: user.id },
-  //     orderBy: {date:"desc"}
-  //   })
-
-  const getLog = (code: string) => {
-    const templates: Record<string, string> = {
-      UPLINK_ESTABLISHED: 'Login successful',
-      PAYMENT_ACCEPTED: 'Transaction authorized',
-      SCHEMATIC_SAVED: 'Hardware schematic saved',
-    }
-
-    return templates[code] || 'Unregistered system event'
-  }
+  const userActivity = await prisma.systemLog.findMany({
+    where: { userId: user.id },
+    orderBy: { createdAt: 'desc' },
+    select: { createdAt: true, action: true, id: true },
+    take: 3,
+  })
 
   return (
     <div>
       <h3 className="text-text-second mb-4 font-bold 2xl:text-xl">
-        <span aria-hidden="true">{'//'} Recent_activity_logs</span>
+        <span aria-hidden="true" className="uppercase">
+          {'//'} Recent_activity_logs
+        </span>
         <span className="sr-only">Recent activity logs</span>
       </h3>
-      <ul className="flex flex-col gap-3">
-        {userActivity.map((activity) => {
-          return (
-            <li
-              key={activity.id}
-              className="text-text-second text-xs font-bold 2xl:text-sm"
-            >
-              <span aria-hidden="true">&gt; </span>
-              {activity.date}: <span aria-hidden="true">{activity.action}</span>{' '}
-              ({getLog(activity.action)})
-            </li>
-          )
-        })}
-      </ul>
+      {userActivity.length > 0 ? (
+        <ul className="flex flex-col gap-3">
+          {userActivity.map((activity) => {
+            return (
+              <li
+                key={activity.id}
+                className="text-text-second text-xs font-bold 2xl:text-sm"
+              >
+                <span aria-hidden="true">&gt; </span>
+                {new Date(activity.createdAt).toLocaleString('pl-PL', {
+                  dateStyle: 'short',
+                  timeStyle: 'short',
+                })}
+                :{' '}
+                <span aria-hidden="true" className="text-accent uppercase">
+                  {activity.action.replace(/\s+/g, '_')}
+                </span>
+                <span className="sr-only">{activity.action}</span>
+              </li>
+            )
+          })}
+        </ul>
+      ) : (
+        <p>
+          <span aria-hidden="true" className="text-accent/90 uppercase">
+            &gt; No_system_activity_detected
+          </span>
+          <span className="sr-only">No user activity</span>
+        </p>
+      )}
       <div className="mt-12 lg:hidden">
         <LogOutButton isAdmin={isAdmin} className="px-0 text-sm" />
       </div>
